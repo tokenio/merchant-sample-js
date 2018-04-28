f75931ef18a96844375f15a6139aaedcbec9c922
Grace <grace09thompson@users.noreply.github.com>
2018-04-28 16:57:51 +0100

    Pl 1537 (#20)
    * use new token request flow
    
    * move js script out of html file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var express = require('express')
-const fs = require('fs')
-var app = express()
+var express = require('express');
+const fs = require('fs');
+var app = express();
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.json({ extended: false });
 
  // Connect to Token's development sandbox
 var TokenLib = require("token-io/dist/token-io.node.js");
@@ -20,21 +20,52 @@ function initServer(member, alias) {
             res.set('Content-Type', 'text/html');
             res.send(contents.replace(/{alias}/g, address));
         })
-    })
+    });
 
     // Endpoint for transferring, called by client side after user approval
     app.post('/transfer', urlencodedParser, function (req, res) {
-        console.log('User request', req.body);
-        // Get the token first and check its validity
-        member.getToken(req.body.tokenId)
-            .then(function (token) {
-                // Redeem the token to move the funds
-                member.redeemToken(token, 4.99, 'EUR', 'Order 123')
-                    .then(function (res) {
-                        console.log('\n Reedeem Token Response:', res);
-                    });
+        const form = req.body;
+
+        // set up the TokenTransferBuilder
+        const tokenBuilder = member.createTransferTokenBuilder(form.amount, form.currency)
+            .setDescription(form.description)
+            .addDestination(form.destination)
+            .setToAlias(alias)
+            .setToMemberId(member.memberId);
+        // set up the TokenRequest
+        const tokenRequest = Token.TokenRequest.create(
+            tokenBuilder.build(),
+            null,
+            null,
+            'http://localhost:3000/redeem'
+        );
+        // store the token request
+        member.storeTokenRequest(tokenRequest).then(function(request) {
+            const requestId = request.id;
+            const redirectUrl = Token.generateTokenRequestUrl(requestId);
+            res.redirect(302, redirectUrl);
+        });
+    });
+
+    app.get('/redeem', urlencodedParser, function (req, res) {
+        //get the token ID from the callback url
+        Token.parseTokenRequestCallbackUrl(req.protocol + '://' + req.get('host') + req.originalUrl)
+            .then(function(tokenId) {
+                console.log(tokenId.tokenId);
+                member.getToken(tokenId.tokenId)
+                    .then(function (token) {
+                        // Redeem the token to move the funds
+                        member.redeemToken(token, 4.99, 'EUR', 'Book Purchase')
+                            .then(function (transfer) {
+                                console.log('\n Reedeem Token Response:', transfer);
+                                res.status(200);
+                                res.redirect('http://localhost:3000');
+                            });
+                    })
             });
     });
+
+    app.use(express.static(__dirname));
     app.listen(3000, function () {
         console.log('Example app listening on port 3000!')
     })
@@ -69,10 +100,10 @@ if (member) {
 } else {
     // Didn't find an existing merchant member. Create a new one.
     const alias = {
-        type: 'EMAIL',
-        value: "msjs-" + Math.random().toString(36).substring(2, 10) + "+noverify@example.com"
+        type: 'DOMAIN',
+        value: "msjs-" + Math.random().toString(36).substring(2, 10) + ".com"
     };
-    Token.createMember(alias, Token.UnsecuredFileCryptoEngine).then(function(m) {
+    Token.createBusinessMember(alias, Token.UnsecuredFileCryptoEngine).then(function(m) {
         member = m;
         // launch server
         initServer(member, alias);

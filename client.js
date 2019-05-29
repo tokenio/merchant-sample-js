488573d34ce8caa294c35997c72f4038a12e1590
grekinsky <greco.rubio@token.io>
2019-05-29 18:12:46 -0500

    Redirect as default, catch up with pfm sample

--- client.js
+++ client.js
@@ -1,11 +1,6 @@
-'use strict';
+"use strict";
 var tokenController;
 var button;
-
-// Client side Token object for creating the Token button, handling the Token Controller, etc
-var token = new window.Token({
-    env: 'sandbox',
-});
 
 function clean() {
     if (button) {
@@ -19,38 +14,46 @@ function clean() {
     }
 }
 
-// set up a function using the item data to populate the request to fetch the TokenRequestFunction
-function getTokenRequestUrl(done) {
-    fetch('/transfer', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({
-            merchantId: 'Merchant 123',
-            amount: 4.99,
-            currency: 'GBP',
-            description: 'Book Purchase',
-            destination: {
-                account: {fasterPayments: {sortCode: '123456', accountNumber: '12345678'}},
-            },
-        }),
-    })
-    .then(function(response) {
-        if (response.ok) {
-            response.text()
-                .then(function(data) {
-                    // execute callback when successful response is received
-                    done(data);
-                    console.log('data: ', data);
-                });
-        }
-    });
-}
-
-function createButton(buttonType) {
+function createRedirectButton() {
     // clean up instances
     clean();
+
+    // Client side Token object for creating the Token button, handling the Token Controller, etc
+    var token = new window.Token({
+        env: 'sandbox',
+    });
+
+    // get button placeholder element
+    var element = document.getElementById('tokenPayBtn');
+
+    // create the button
+    button = token.createTokenButton(element, {
+        label: 'Token Quick Checkout',
+    });
+
+    // create TokenController to handle messages
+    tokenController = token.createController();
+
+    // bind the Token Button to the Token Controller when ready
+    tokenController.bindButtonClick(
+        button, // Token Button
+        redirectTokenRequest, // redirect token request function
+        function(error) { // bindComplete callback
+            if (error) throw error;
+            // enable button after binding
+            button.enable();
+        },
+    );
+}
+
+function createPopupButton() {
+    // clean up instances
+    clean();
+
+    // Client side Token object for creating the Token button, handling the Token Controller, etc
+    var token = new window.Token({
+        env: 'sandbox',
+    });
 
     // get button placeholder element
     var element = document.getElementById('tokenPayBtn');
@@ -64,7 +67,7 @@ function createButton(buttonType) {
     tokenController = token.createController({
         onSuccess: function(data) { // Success Callback
             // build success URL
-            var successURL = `/redeem?data=${window.encodeURIComponent(JSON.stringify(data))}`;
+            var successURL = `/redeem-popup?data=${window.encodeURIComponent(JSON.stringify(data))}`;
             // navigate to success URL
             window.location.assign(successURL);
         },
@@ -75,17 +78,59 @@ function createButton(buttonType) {
 
     // bind the Token Button to the Token Controller when ready
     tokenController.bindButtonClick(
-        button, // TokenButtonController
+        button, // Token Button
         getTokenRequestUrl, // token request function
         function(error) { // bindComplete callback
-            // enable button after binding
             if (error) throw error;
+            // enable button after binding
             button.enable();
         },
         { // options
-            desktop: buttonType,
+            desktop: 'POPUP',
         }
     );
+}
+
+function redirectTokenRequest() {
+    var data = {
+        amount: 4.99,
+        currency: 'EUR',
+        description: 'Book Purchase',
+    };
+
+    // format data as URL query string
+    var queryString = Object.keys(data).map(key => key + '=' + window.encodeURIComponent(data[key])).join('&');
+
+    // go to transfer
+    document.location.assign("/transfer?" + queryString);
+}
+
+// set up a function using the item data to populate the request to fetch the TokenRequestFunction
+function getTokenRequestUrl(done) {
+    var data = {
+        amount: 4.99,
+        currency: 'EUR',
+        description: 'Book Purchase',
+    };
+
+    // fetch Token Request URL
+    fetch('/transfer-popup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(function(response) {
+        if (response.ok) {
+            response.text()
+                .then(function(data) {
+                    // execute callback when successful response is received
+                    done(data);
+                    console.log('data: ', data);
+                });
+        }
+    });
 }
 
 function setupButtonTypeSelector() {
@@ -99,10 +144,14 @@ function setupButtonTypeSelector() {
             var value = e.target.value;
             if (value === selected) return;
             selected = value;
-            createButton(value);
+            if (value === 'POPUP') {
+                createPopupButton();
+            } else if (value === 'REDIRECT') {
+                createRedirectButton();
+            }
         });
     }
-    createButton();
+    createRedirectButton();
 }
 
 setupButtonTypeSelector();

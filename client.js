a000ac530f2624497c3f6794423788de45c3e224
priyanshu <priyanshu@appperfect.com>
2020-03-04 17:31:23 +0530

    Review commit fix

--- client.js
+++ client.js
@@ -20,58 +20,23 @@ function clean() {
     }
 }
 
-function createRedirectButton(transferType) {
-    // clean up instances
-    clean();
-
-    // Client side Token object for creating the Token button, handling the Token Controller, etc
-    var token = new window.Token({
-        env: 'sandbox',
-    });
-
-    // get button placeholder element
-    var element = document.getElementById('tokenPayBtn');
-
-    // create the button
-    button = token.createTokenButton(element, {
-        label: 'Token Quick Checkout',
-    });
-
-    // create TokenController to handle messages
-    tokenController = token.createController();
-
-    // bind the Token Button to the Token Controller when ready
-    tokenController.bindButtonClick(
-        button, // Token Button
-        token.createRequest(() => {
-            redirectTokenRequest(transferType) // redirect token request function
-        }),        
-        async error => { // bindComplete callback
-            if (error) throw error;
-            // enable button after binding
-            button.enable();
-        },
-    );
+function tokenRedirectPath() {
+    var path = '';
+    if (selectedTransferType === 'STANDING_ORDER') {
+        path = '/standing-order?';
+    } else if (selectedTransferType === 'FUTURE_DATED') {
+        path = '/future-dated?';
+    } else if (selectedTransferType === 'ONE_STEP') {
+        path = '/one-step-payment?';
+    } else if (selectedTransferType === 'CROSS_BORDER') {
+        path = '/cross-border?';
+    } else {
+        path = '/transfer?'
+    }
+    return path;
 }
 
-function createPopupButton() {
-    // clean up instances
-    clean();
-
-    // Client side Token object for creating the Token button, handling the Token Controller, etc
-    var token = new window.Token({
-        env: 'sandbox',
-    });
-
-    // get button placeholder element
-    var element = document.getElementById('tokenPayBtn');
-
-    // create the button
-    button = token.createTokenButton(element, {
-        label: 'Token Quick Checkout',
-    });
-
-    // create TokenController to handle messages
+function redeemTokenPopUp() {
     var path = "";
     if (selectedTransferType === 'STANDING_ORDER') {
         path = '/redeem-standing-order-popup';
@@ -84,85 +49,23 @@ function createPopupButton() {
     } else {
         path = '/redeem-popup';
     }
-    tokenController = token.createController({
-        onSuccess: function (data) { // Success Callback
-            // build success URL
-            var successURL = `${path}?data=${window.encodeURIComponent(JSON.stringify(data))}`;
-            // navigate to success URL
-            window.location.assign(successURL);
-        },
-        onError: function (error) { // Failure Callback
-            throw error;
-        },
-    });
-
-    // bind the Token Button to the Token Controller when ready
-    tokenController.bindButtonClick(
-        button, // Token Button
-        getTokenRequestUrl, // token request function
-        function (error) { // bindComplete callback
-            if (error) throw error;
-            // enable button after binding
-            button.enable();
-        },
-        { // options
-            desktop: 'POPUP',
-        }
-    );
+    return path;
 }
 
-function redirectTokenRequest(transferType) {
-    // format data as URL query string
-    var queryString = Object.keys(data).map(key => key + '=' + window.encodeURIComponent(data[key])).join('&');
-
-    // go to transfer
+function tokenPopupPath() {
     var path = "";
     if (selectedTransferType === 'STANDING_ORDER') {
-        path = '/standing-order?';
+        path = '/standing-order-popup?';
     } else if (selectedTransferType === 'FUTURE_DATED') {
-        path = '/future-dated?';
+        path = '/future-dated-popup?';
     } else if (selectedTransferType === 'ONE_STEP') {
-        path = '/one-step-payment?';
+        path = '/one-step-payment-popup?';
     } else if (selectedTransferType === 'CROSS_BORDER') {
-        path = '/cross-border?';
+        path = '/cross-border-popup?';
     } else {
-        path = '/transfer?'
+        path = '/transfer-popup?';
     }
-    document.location.assign(path + queryString);
-}
-
-// set up a function using the item data to populate the request to fetch the TokenRequestFunction
-function getTokenRequestUrl(done) {
-    // fetch Token Request URL
-    var path = "";
-    if (selectedTransferType === 'STANDING_ORDER') {
-        path = '/standing-order-popup';
-    } else if (selectedTransferType === 'FUTURE_DATED') {
-        path = '/future-dated-popup';
-    } else if (selectedTransferType === 'ONE_STEP') {
-        path = '/one-step-payment-popup';
-    } else if (selectedTransferType === 'CROSS_BORDER') {
-        path = '/cross-border-popup';
-    } else {
-        path = '/transfer-popup';
-    }
-    fetch(path, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify(data),
-    })
-        .then(function (response) {
-            if (response.ok) {
-                response.text()
-                    .then(function (data) {
-                        // execute callback when successful response is received
-                        done(data);
-                        console.log('data: ', data);
-                    });
-            }
-        });
+    return path;
 }
 
 function setupButtonTypeSelector() {
@@ -193,10 +96,81 @@ function setupButtonTypeSelector() {
 
 function createTokenRequestButton(selectedMode) {
     if (selectedMode === 'POPUP') {
-        createPopupButton();
+        createTokenButton('POPUP')
     } else if (selectedMode === 'REDIRECT') {
-        createRedirectButton();
+        createTokenButton('REDIRECT');
     }
+}
+
+function createTokenButton(type) {
+    // clean up instances
+    clean();
+
+    // Client side Token object for creating the Token button, handling the Token Controller, etc
+    var token = new window.Token({
+        env: 'sandbox',
+    });
+
+    // get button placeholder element
+    var element = document.getElementById('tokenPayBtn');
+    const webapp = type === 'POPUP' ? true : false;
+
+    // create the button
+    button = token.createTokenButton(element, {
+        label: 'Token Quick Checkout',
+    });
+
+    var path = (type === 'POPUP') ? tokenPopupPath() : tokenRedirectPath();
+    var queryString = Object.keys(data).map(key => key + '=' + window.encodeURIComponent(data[key])).join('&');
+
+    tokenController = token.createController({
+        onSuccess: function (data) { // Success Callback
+            // build success URL
+            var successURL = `${redeemTokenPopUp()}?data=${window.encodeURIComponent(JSON.stringify(data))}`;
+            // navigate to success URL
+            window.location.assign(successURL);
+        },
+        onError: function (error) { // Failure Callback
+            throw error;
+        },
+    });
+
+    // bind the Token Button to the Token Controller when ready
+    tokenController.bindButtonClick(
+        button, // Token Button
+        token.createRequest(
+            redirect => {
+                window.location.assign(path + queryString);
+            },
+            async (done, error) => {
+                fetch(path, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8',
+                    },
+                    body: JSON.stringify(data),
+                })
+                    .then(function (response) {
+                        if (response.ok) {
+                            response.text()
+                                .then(function (data) {
+                                    // execute callback when successful response is received
+                                    done(data);
+                                    console.log('data: ', data);
+                                });
+                        }
+                    });
+            }
+        ), // token request function
+        function (error) { // bindComplete callback
+            if (error) throw error;
+            // enable button after binding
+            button.enable();
+        },
+        { // options
+            desktop: webapp ? 'POPUP' : 'REDIRECT',
+        }
+    );
 }
 
 setupButtonTypeSelector();
